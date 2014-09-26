72a0f541e69b8ac6151a329d58881a36c560149c
mammenx <mammenx@gmail.com>
2014-09-26 16:24:44 +0530

    Initial templates

---
+++ templates/peg_ovm_template_seq.sv
@@ -0,0 +1,73 @@
+/*
+ --------------------------------------------------------------------------
+   Synesthesia-Moksha - Copyright (C) 2012 Gregory Matthew James.
+
+   This file is part of Synesthesia-Moksha.
+
+   Synesthesia-Moksha is free; you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   Synesthesia-Moksha is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+*/
+
+/*
+ --------------------------------------------------------------------------
+ -- Project Code      : synesthesia-moksha
+ -- Sequence Name     : <seq_name>
+ -- Author            : 
+ -- Function          :
+ --------------------------------------------------------------------------
+*/
+
+`ifndef __<seq_name>
+`define __<seq_name>
+
+  class <seq_name>   #(
+                        type  PKT_TYPE  = ,
+                        type  SEQR_TYPE =
+                     ) extends ovm_sequence  #(PKT_TYPE);
+
+    /*  Adding the parameterized sequence to the registery  */
+    typedef <seq_name>#(PKT_TYPE,SEQR_TYPE) this_type;
+    typedef ovm_object_registry#(this_type)type_id;
+
+    /*  Linking with p_sequencer  */
+    `ovm_declare_p_sequencer(SEQR_TYPE)
+
+
+    /*  Constructor */
+    function new(string name  = "<seq_name>");
+      super.new(name);
+    endfunction
+
+    /*  Body of sequence  */
+    task  body();
+      p_sequencer.ovm_report_info(get_name(),"Start of <seq_name>",OVM_LOW);
+
+    endtask : body
+
+
+  endclass  : <seq_name>
+
+`endif
+
+/*
+ --------------------------------------------------------------------------
+
+ -- <Header>
+ 
+
+ -- <Log>
+
+
+ --------------------------------------------------------------------------
+*/
